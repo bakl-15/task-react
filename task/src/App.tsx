2ae1f880030e79fffe88dd617e4a87c1d55dfb0b
bakl-15 <74142101+bakl-15@users.noreply.github.com>
2025-11-14 00:06:21 +0100

    Update App.tsx

--- task/src/App.tsx
+++ task/src/App.tsx
@@ -1,11 +1,36 @@
 
-
+type Priority = "urgente" | "Moyenne" | "Basse"
+type todo = {
+  id: number,
+  text: string,
+  priority: Priority
+}
 function App() {
 
 
   return (
     <>
-       <h1 className="text-3xl font-bold underline bg-red-500">      Hello world!    </h1>
+       <div className="flex justify-center">
+          <div className="w-2/3 flex flex-col gap-4 my-15 bg-base-300 rounded-2xl p-5">
+               <div className="flex gap-4">
+                 <input 
+                    type="text"
+                    className="input w-full"
+                    placeholder="Ajouter une tache"
+                 />
+                 <select 
+                   className="select w-full"
+                 >
+                     <option value="urgente">urgente</option> 
+                     <option value="moyenne">moyenne</option> 
+                     <option value="basse">basse</option> 
+                 </select>
+                 <button className="btn btn-primary">
+                     Ajouter
+                 </button>
+               </div>
+          </div>.
+       </div>
     </>
   )
 }
